6637c062dd553505be5de85ccda424e7e25c9dc7
rgrascarmona <rgrascarmona@gmail.com>
2024-01-31 07:21:13 +0100

    First commit

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+
+	
+}
---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ramon', 'Gras Carmona', 'rgrascarmona@gmail.com','2024-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Elisabet', 'Solis Casas', 'esolis@gmail.com','2024-01-20');
